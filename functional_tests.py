e7034b4edfb76718b322671dcf156b4be6fd72ab
Max Caldwell <maxcaldwell1@gmail.com>
2014-07-06 15:11:46 -0700

    Created first FT and got an expected failure

--- functional_tests.py
+++ functional_tests.py
@@ -11,8 +11,23 @@ class TestLandingPage(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_nothing(self):
-        self.assertTrue(1==2)
+    #First user lands on landing page and decides to create an account and sign in
+    def test_landing_page_can_create_account_and_redirect(self):
+
+        class User:
+            def __init__(self, name, password):
+                self.name = name
+                self.password = password
+
+        account_info = self.browser.find_element_by_tag_name("account_info")
+        user_name = account_info.find_element_by_tag_name("user_name")
+        password = account_info.find_element_by_tag_name("pass_field")
+
+        bob = User("Killer BOB")
+
+        user_name.send_keys(bob.name)
+        password.send_keys(bob.password, Keys.ENTER)
+
 
 if __name__ == '__main__':
     unittest.main()
