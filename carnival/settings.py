0107f4a6ef29c8a29e37aba61c48148c66a704de
Max Caldwell <maxcaldwell1@gmail.com>
2014-07-06 16:27:31 -0700

    wrote first failing unit test

--- carnival/settings.py
+++ carnival/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'group',
 )
 
 MIDDLEWARE_CLASSES = (
--- group/tests.py
+++ group/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from group.views import landing_page
 
 # Create your tests here.
+class LandingViewTester(TestCase):
+
+    def test_landing_page_exists_and_is_using_correct_template(self):
+
+        c = Client()
+
+        response = c.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response.templates, 'base.html')
