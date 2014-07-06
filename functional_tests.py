429f8726634d7cda8495bc72e0a744008d6ae130
Max Caldwell <maxcaldwell1@gmail.com>
2014-07-06 14:48:59 -0700

    got started with a trivial FT failure

--- functional_tests.py
+++ functional_tests.py
@@ -1,1 +1,18 @@
-import selenium
+from selenium import webdriver
+import unittest
+
+class TestLandingPage(unittest.TestCase):
+
+    browser = webdriver.Chrome()
+
+    def setUp(self):
+        self.browser.get('http://localhost:8000/')
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_nothing(self):
+        self.assertTrue(1==2)
+
+if __name__ == '__main__':
+    unittest.main()
